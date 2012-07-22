eedecd9cd650f3d788c6838c272c6bbbf4847e20
jant <tjanousek@volny.cz>
2012-07-22 23:14:50 +0200

    doreseni err. msg. pri smazani specializace, ktera ma pod sebou ordinaci

--- rails/esestra/app/models/specialty.rb
+++ rails/esestra/app/models/specialty.rb
@@ -1,4 +1,6 @@
 class Specialty < ActiveRecord::Base
+  has_many :surgeries, dependent: :restrict
+
   validates_uniqueness_of :name
   validates_presence_of :name
   validates_length_of :name, maximum: 40
--- rails/esestra/test/unit/specialty_test.rb
+++ rails/esestra/test/unit/specialty_test.rb
@@ -12,18 +12,31 @@ class SpecialtyTest < ActiveSupport::TestCase
   test 'must be invalid if name is not unique' do
     specialty = Specialty.new(name: 'ORL')
     assert specialty.invalid?
-    assert_equal 'Specializace se jménem="ORL" již je evidována', specialty.errors[:name].join
+    assert_equal 'Specializace se jménem="ORL" již je evidována', specialty.errors[:name].first
   end
 
-  test 'must be invalid if required attributes are not present' do
+  test 'must be invalid if requiresd attributes are not present' do
     specialty = Specialty.new
     assert specialty.invalid?
-    assert_equal 'Jméno specializace je povinné', specialty.errors[:name].join
+    assert_equal 'Jméno specializace je povinné', specialty.errors[:name].first
   end
 
   test 'must be invalid if name is too long' do
     specialty = Specialty.new(name: 'x' * 41)
     assert specialty.invalid?
-    assert_equal 'Jméno specializace může obsahovat maximálně 40 znaků', specialty.errors[:name].join
+    assert_equal 'Jméno specializace může obsahovat maximálně 40 znaků', specialty.errors[:name].first
+  end
+
+  test 'must not destroy if has children' do
+    # ve verzi rails 3.2.6 destroy pri poruseni dependent restrict zveda vyjimku
+    # v dalsich verzich rails jiz nebude zvedat vyjimku, ale bude zapisovat do errors[:base] danou chybu. Mozna to bude zaviset na parametru dependent_restrict_raises...
+    # take by melo byt mozne chybove hlaseni osetrit pomoci I18n
+    specialty = specialties('ORL')
+    begin
+      specialty.destroy
+    rescue ActiveRecord::DeleteRestrictionError => e
+      assert_equal "Cannot delete record because of dependent surgeries", e.message
+    end
+    assert_equal 3, Specialty.count
   end
 end
