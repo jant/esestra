60121f2b47ea619bd61ff7d338e384f881b3920a
jant <tjanousek@volny.cz>
2012-08-15 23:17:14 +0200

    zjednoduseni css a dalsi pokusy

--- rails/esestra/app/controllers/specialties_controller.rb
+++ rails/esestra/app/controllers/specialties_controller.rb
@@ -4,4 +4,21 @@ class SpecialtiesController < ApplicationController
   def index
     @specialties = Specialty.order(:name).all
   end
+
+  def new
+    @specialty = Specialty.new
+  end
+
+  def create
+    @specialty = Specialty.new(params[:specialty])
+
+    respond_to do |format|
+      if @specialty.save
+        format.html { render action: "edit" }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
 end
