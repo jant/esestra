c34af6010cfa44f56c8278cb104bf4fc6e6f7622
jant <tjanousek@volny.cz>
2012-08-16 23:05:03 +0200

    dotazeni crud pro specializace

--- rails/esestra/app/controllers/specialties_controller.rb
+++ rails/esestra/app/controllers/specialties_controller.rb
@@ -1,24 +1,37 @@
 class SpecialtiesController < ApplicationController
-  respond_to :html
-
   def index
     @specialties = Specialty.order(:name).all
   end
 
+  def show
+    redirect_to edit_specialty_path, id: params[:id]
+  end
+
   def new
     @specialty = Specialty.new
+    render :form
+  end
+
+  def edit
+    @specialty = Specialty.find(params[:id])
+    render :form
   end
 
   def create
-    @specialty = Specialty.new(params[:specialty])
+    @specialty = Specialty.create(params[:specialty])
+    render :form
+  end
 
-    respond_to do |format|
-      if @specialty.save
-        format.html { render action: "edit" }
-      else
-        format.html { render action: "new" }
-      end
-    end
+  def update
+    @specialty = Specialty.find(params[:id])
+    @specialty.update_attributes(params[:specialty])
+    render :form
+  end
+
+  def destroy
+    @specialty = Specialty.find(params[:id])
+    @specialty.destroy
+    redirect_to specialties_path
   end
 
 end
